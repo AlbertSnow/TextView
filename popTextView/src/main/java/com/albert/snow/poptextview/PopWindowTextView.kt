4b9b7fec0d877fad17d362397d5921c82771bf5e
赵佳亮 <zhaojialiang@sogou-inc.com>
2020-10-09 10:20:50 +0800

    first commit

---
+++ popTextView/src/main/java/com/albert/snow/poptextview/PopWindowTextView.kt
@@ -0,0 +1,17 @@
+package com.albert.snow.poptextview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class PopWindowTextView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+
+    init {
+
+    }
+
+
+}
