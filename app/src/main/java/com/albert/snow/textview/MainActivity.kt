e6919a07251d11e3cbb5df942ab409800a8e4100
赵佳亮 <zhaojialiang@sogou-inc.com>
2020-10-09 18:03:56 +0800

    Add clickable span test

--- app/src/main/java/com/albert/snow/textview/MainActivity.kt
+++ app/src/main/java/com/albert/snow/textview/MainActivity.kt
@@ -2,7 +2,12 @@ package com.albert.snow.textview
 
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jaeger.library.OnSelectListener
 import com.jaeger.library.SelectableTextHelper
@@ -30,5 +35,24 @@ class MainActivity : AppCompatActivity() {
                 .build()
 
         mSelectableTextHelper?.setSelectListener(OnSelectListener { })
+
+
+        addClickableSpan(mTvTest!!)
+        addClickableSpan(findViewById(R.id.main_custom_tv))
     }
+
+    private fun addClickableSpan(textView: TextView) {
+        val span = object: ClickableSpan() {
+            override fun onClick(widget: View) {
+                Toast.makeText(widget.context, "Hello Clickable", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val text = SpannableStringBuilder(textView.text)
+        text.setSpan(span, 0, 10, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textView.text = text
+
+    }
+
 }
