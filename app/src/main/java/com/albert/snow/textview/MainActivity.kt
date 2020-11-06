bcde413866ad4d4bb208506f191357ccb9855a96
赵佳亮 <zhaojialiang@sogou-inc.com>
2020-11-06 11:35:42 +0800

    1. Add pop window to test

--- app/src/main/java/com/albert/snow/textview/MainActivity.kt
+++ app/src/main/java/com/albert/snow/textview/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ClickableSpan
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +16,10 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private var mTvTest: TextView? = null
+    private var mContentView: View? = null
+
+    lateinit var mPopWindow: PopupWindow
+    lateinit var popContentView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +28,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        mTvTest = findViewById(R.id.main_second_tv) as TextView
+        mContentView = this.window.decorView.findViewById(android.R.id.content)
+        mTvTest = findViewById(R.id.main_second_tv)
         addClickableSpan(mTvTest!!)
         addClickableSpan(findViewById(R.id.main_custom_tv))
+
+        initPopContentView()
+        initPopWindow();
+    }
+
+    private fun initPopContentView() {
+        popContentView = LayoutInflater.from(baseContext).inflate(R.layout.pop_item, null, false)
+
+        popContentView.findViewById<View>(R.id.main_update_btn)?.setOnClickListener {
+            mPopWindow.update(0, 20, -1, -1, true)
+        }
+
+        popContentView.findViewById<View>(R.id.main_show_at_btn)?.setOnClickListener {
+        }
+    }
+
+    private fun initPopWindow() {
+        mPopWindow = PopupWindow(
+            popContentView,
+            60,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
     }
 
     private fun addClickableSpan(textView: TextView) {
-        val span = object: ClickableSpan() {
+        val span = object : ClickableSpan() {
             override fun onClick(widget: View) {
-                Toast.makeText(widget.context, "Hello Clickable", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -37,5 +67,33 @@ class MainActivity : AppCompatActivity() {
 
         textView.text = text
     }
+
+    override fun onBackPressed() {
+        if (mPopWindow.isShowing) {
+            mPopWindow.dismiss()
+        }
+        var x = 0;
+        var y = 50;
+        mPopWindow.showAtLocation(mTvTest, Gravity.CENTER, x, y)
+    }
+
+    fun popLeft(view: View) {
+        if (mPopWindow.isShowing) {
+            mPopWindow.dismiss()
+        }
+        var x = 0;
+        var y = 50;
+        mPopWindow.showAtLocation(mTvTest, Gravity.LEFT, x, y)
+    }
+
+    fun popRight(view: View) {
+        if (mPopWindow.isShowing) {
+            mPopWindow.dismiss()
+        }
+        var x = 0;
+        var y = 50;
+        mPopWindow.showAtLocation(mTvTest, Gravity.RIGHT, x, y)
+    }
+
 
 }
